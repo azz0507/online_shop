3cd2af9dde35feb2ce9c8899268251ebe51277b0
azz0507 <azizbekazar10@gmail.com>
2024-06-14 15:54:19 -0700

    14 iyun ( login bilan )

--- sports/urls.py
+++ sports/urls.py
@@ -1,15 +1,18 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
 from sports.views import index
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from sports import views
+
+# my_projects/sports/urls.py
+
 
 urlpatterns = [
-    path('', index, name='index')
-
+    path('login/', auth_views.LoginView.as_view(template_name='sports/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.index, name='index'),  # ваша домашняя страница
 ]
-
-from django.contrib import admin
-from django.urls import path, include
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- sports/views.py
+++ sports/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from sports.models import Product
+from django.contrib.auth.views import LoginView
+from .forms import LoginForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -19,3 +26,13 @@ def detail(request, pk):
     product = Product.objects.get(pk=pk)
     context = {'product': product}
     return render(request, 'sports/detail.html', context)
+
+
+# sports/views.py
+
+# my_projects/sports/views.py
+
+
+@login_required
+def home(request):
+    return render(request, 'sports/home.html')
