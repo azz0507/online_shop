8af6946c680ea71b453f1b108606407e1af2c1e0
azz0507 <azizbekazar10@gmail.com>
2024-06-14 10:35:16 -0700

    12 iyun

--- sports/urls.py
+++ sports/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from sports.views import index
 
@@ -6,3 +8,8 @@ urlpatterns = [
 
 ]
 
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
